35aa7870cb1749fab1eccb7209a6386301eb36ca
Jerry Zhang <jerryzh168@gmail.com>
2024-11-25 15:33:23 -0800

    Extend gpt-fast LLM dashboard to support torchao autoquant
    Summary:
    We want to test autoquant on relevant LLM models
    
    right now only llama2 and mixtral, but want to extend to more models like https://github.com/vllm-project/vllm/tree/main/vllm/model_executor/models
    
    Test Plan:
    https://hud.pytorch.org/benchmark/llms?repoName=pytorch%2Fpytorch
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: f5b7aa2087cf4bb6432e2c1131c7006e8eb334fe
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/140627

--- benchmarks/gpt_fast/benchmark.py
+++ benchmarks/gpt_fast/benchmark.py
@@ -268,6 +268,10 @@ all_experiments = {
     run_llama2_7b_bf16,
     run_llama2_7b_int8,
     run_mixtral_8x7b_int8,
+    # waiting for A100-80G machine to be available in CI
+    # https://github.com/pytorch/pytorch/actions/runs/12018005803/job/33503683582?pr=140627
+    # run_mixtral_8x7b_autoquant,
+    # run_llama2_7b_autoquant,
     # A list of micro-benchmarks.
     run_mlp_layer_norm_gelu,
     run_layer_norm,
@@ -286,6 +290,7 @@ def main(output_file=DEFAULT_OUTPUT_FILE):
             # This happens when torch is compiled with CUDA turning off completely
             device = "cpu"
 
+        torch.compiler.cudagraph_mark_step_begin()
         lst = func(device)
         for x in lst:
             results.append(dataclasses.astuple(x))
